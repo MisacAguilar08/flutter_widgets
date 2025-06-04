0529b4533e746c3b1fa7866bc7070147a8f9338e
MisacAguilar08 <misacaguilar@gmail.com>
2025-06-04 11:29:17 -0600

    cards elevation

--- lib/presentation/screens/cars/cards_screen.dart
+++ lib/presentation/screens/cars/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
   @override
@@ -8,13 +17,47 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards Screen'),
       ),
-      body: Center(
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text('This is a card'),
-          ),
-        ),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: [
+        ...cards.map((card) =>
+            _CardTpe1(label: card['label'], elevation: card['elevation'])),
+      ]),
+    );
+  }
+}
+
+class _CardTpe1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardTpe1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+              alignment: Alignment.topRight,
+              child: IconButton(onPressed: (){}, icon: Icon(Icons.more_vert_outlined))),
+          Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+          )
+        ]),
       ),
     );
   }
